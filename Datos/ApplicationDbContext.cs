5966a9962d27b9b4dd205de61f807dc3c84bcc05
emmanuelespi <eespinest87@gmail.com>
2023-10-29 23:13:49 -0600

    Primer commit 29102023
    Primer commit

---
+++ Datos/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CrudNet7MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudNet7MVC.Datos
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        
+        //Debemos agregar los modelos
+        public DbSet<Contacto> Contacto { get; set; }
+    }
+}
---
+++ Models/Contacto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudNet7MVC.Models
+{
+    public class Contacto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El nombre es obligatorio")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage ="El teléfono es obligatorio")]
+        public string Telefono { get; set; }
+        [Required(ErrorMessage = "El telefono es obligatorio")]
+        public string Celular { get; set; }
+        [Required(ErrorMessage = "El teléfono celular es obligatorio")]
+        public string Email { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+    }
+}
